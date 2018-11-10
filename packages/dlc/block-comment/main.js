96d4a599c2f6197b1f71f1a59f0f2f38be100fda
everettjf <everettjf@live.com>
2018-11-11 01:39:30 +0800

    Update packages/dlc/block-comment/main.js

--- packages/dlc/block-comment/main.js
+++ packages/dlc/block-comment/main.js
@@ -1,9 +1,15 @@
 
 
 var onMenuClicked = function(identifier){
-
+    var lines = invocation.selectionLines;
+    if(lines.length == 0){
+        return {
+            'result':false,
+            'error':'No lines selected'
+        };
+    }
+    
     system.log(invocation.selections);
-
 
     return {'result':true};
 }
