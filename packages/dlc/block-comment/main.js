009d15b22b695331af4306b6044218fd86583f46
everettjf <everettjf@live.com>
2018-11-11 02:06:42 +0800

    Update packages/dlc/block-comment/main.js

--- packages/dlc/block-comment/main.js
+++ packages/dlc/block-comment/main.js
@@ -1,15 +1,34 @@
 
 
 var onMenuClicked = function(identifier){
-    var lines = invocation.selectionLines;
-    if(lines.length == 0){
+    system.log(invocation.selections);
+    if(!invocation.selectionExist){
         return {
-            'result':false,
-            'error':'No lines selected'
+            'result':false
         };
     }
+    var firstsel = invocation.selections[0];
     
-    system.log(invocation.selections);
+    var startline = firstsel[0];
+    var startcol = firstsel[1];
+    var endline = firstsel[2];
+    var endcol = firstsel[3];
+    
+    var lines = invocation.lines;
+    var startcontent = lines[startline];
+    var endcontent = lines[endline];
+
+    if(startline == endline){
+        var newstart = startcontent.substring(0,startcol) + "/*" + startcontent.substring(startcol,endcol)
+                + "*/" + endcontent.substring(endcol,endcontent.length);
+        invocation.assignLineAtIndex(newstart,startline);
+
+    } else{
+        var newstart = startcontent.substring(0,startcol) + "/*" + startcontent.substring(startcol,startcontent.length);
+        var newend = endcontent.substring(0,endcol) + "*/" + endcontent.substring(endcol,endcontent.length);
+        invocation.assignLineAtIndex(newstart,startline);
+        invocation.assignLineAtIndex(newend,endline);
+    }
 
     return {'result':true};
 }
