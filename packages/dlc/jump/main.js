3ccafff7b56caf4b678da224672545d03f36a066
everettjf <everettjf@live.com>
2018-11-11 02:19:18 +0800

    Update packages/dlc/jump/main.js

--- packages/dlc/jump/main.js
+++ packages/dlc/jump/main.js
@@ -1,5 +1,40 @@
 
 var onMenuClicked = function(identifier){
+    system.log(invocation.selections);
+    if(invocation.selectionExist){
+        return {
+            'result':false
+        };
+    }
+
+    var firstSelection = invocation.firstSelection;
+
+    if(identifier == 'jump.up-20-lines'){
+        var startline = firstSelection[0];
+        var startcol = firstSelection[1];
+
+        startline -= 20;
+        if(startline < 0){
+            startline = 0;
+        }
+
+        invocation.selections = [[startline,startcol,startline,startcol]];
+
+    }else if(identifier == 'jump.down-20-lines'){
+        var startline = firstSelection[0];
+        var startcol = firstSelection[1];
+
+        startline += 20;
+        if(startline >= invocation.lineCount){
+            startline = invocation.lineCount -1;
+        }
+
+        invocation.selections = [[startline,startcol,startline,startcol]];
+    }else{
+        return {
+            'result':false
+        };
+    }
 
     return {'result':true};
 }
